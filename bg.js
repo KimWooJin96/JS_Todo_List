ac78c09b5aaaa344ab0e1127f8807eb25f61b53f
Woojin <sam4woojin@naver.com>
2021-02-10 18:16:04 +0900

    JS_Background

---
+++ bg.js
@@ -0,0 +1,22 @@
+const body = document.querySelector("body");
+
+const IMG_NUM = 3;
+
+function applyImage(randomNum) {
+  const image = new Image();
+  image.src = `/images/${randomNum + 1}.jpg`;
+  image.classList.add("bgImage");
+  body.appendChild(image);
+}
+
+function getRandom() {
+  const num = Math.floor(Math.random() * IMG_NUM);
+  return num;
+}
+
+function init() {
+  const randomNum = getRandom();
+  applyImage(randomNum);
+}
+
+init();
