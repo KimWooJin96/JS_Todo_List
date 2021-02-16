de9cae097a31f3f7f1fdcbe929b63f6a4bd658d1
Woojin <sam4woojin@naver.com>
2021-02-16 18:08:57 +0900

    CSS_ListSpace

--- todo.js
+++ todo.js
@@ -23,15 +23,6 @@ function countToDo() {
   }
 }
 
-function removeRemainToDo(moveToDos) {
-  console.log(moveToDos[0]);
-  const lis = toDoListUncomplete.querySelectorAll("li");
-  lis.forEach((li) => {
-    const a = li.index === moveToDos[0].index;
-    console.log(a);
-  });
-}
-
 function removeToDoComplete(event) {
   const target = event.target;
   const li = target.parentNode;
@@ -42,20 +33,6 @@ function removeToDoComplete(event) {
   });
   toDosDone = cleanToDos;
   saveToDoComplete();
-  countToDo();
-}
-
-function removeToDo(event) {
-  const target = event.target;
-  const buttonContainer = target.parentNode;
-  const li = buttonContainer.parentNode;
-  toDoListUncomplete.removeChild(li);
-
-  const cleanToDos = toDos.filter((toDo) => {
-    return toDo.index !== parseInt(li.id);
-  });
-  toDos = cleanToDos;
-  saveToDo();
   countToDo();
 }
 
@@ -74,8 +51,25 @@ function saveToDoComplete() {
   localStorage.setItem(TODO_COMPLETE, JSON.stringify(toDosDone));
 }
 
-function saveToDo() {
-  localStorage.setItem(TODO_VALUE, JSON.stringify(toDos));
+function paintToDoCompleteSaved(key, index) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  const buttonExit = document.createElement("button");
+  newToDo = {
+    index,
+    key,
+  };
+  span.innerText = `${key}`;
+  buttonExit.innerText = "❌";
+  buttonExit.id = "button__exit";
+  li.appendChild(span);
+  li.appendChild(buttonExit);
+  li.id = index;
+  toDoListComplete.appendChild(li);
+  toDosDone.push(newToDo);
+  // console.log(toDos);
+  saveToDoComplete();
+  buttonExit.addEventListener("click", removeToDoComplete);
 }
 
 function paintToDoComplete(moveToDos) {
@@ -100,57 +94,22 @@ function paintToDoComplete(moveToDos) {
   buttonExit.addEventListener("click", removeToDoComplete);
 }
 
-function paintToDoInitial(currentToDo) {
-  const li = document.createElement("li");
-  const span = document.createElement("span");
-  const div = document.createElement("div");
-  const buttonComplete = document.createElement("button");
-  const buttonExit = document.createElement("button");
-  const index = Date.now();
-  newToDo = {
-    index: index,
-    key: currentToDo,
-  };
-  span.innerText = `${currentToDo}`;
-  buttonComplete.innerText = "✔️";
-  buttonComplete.id = "button__complete";
-  buttonExit.innerText = "❌";
-  buttonExit.id = "button__exit";
-  li.appendChild(span);
-  li.appendChild(buttonComplete);
-  li.appendChild(div);
-  div.appendChild(buttonComplete);
-  div.appendChild(buttonExit);
-  div.id = "buttonContainer";
-  li.id = index;
-  toDoListUncomplete.appendChild(li);
-  toDos.push(newToDo);
-  // console.log(toDos);
+function removeToDo(event) {
+  const target = event.target;
+  const buttonContainer = target.parentNode;
+  const li = buttonContainer.parentNode;
+  toDoListUncomplete.removeChild(li);
+
+  const cleanToDos = toDos.filter((toDo) => {
+    return toDo.index !== parseInt(li.id);
+  });
+  toDos = cleanToDos;
   saveToDo();
-  buttonExit.addEventListener("click", removeToDo);
-  buttonComplete.addEventListener("click", completeToDo);
-  buttonComplete.addEventListener("click", removeToDo);
+  countToDo();
 }
 
-function paintToDoCompleteSaved(key, index) {
-  const li = document.createElement("li");
-  const span = document.createElement("span");
-  const buttonExit = document.createElement("button");
-  newToDo = {
-    index,
-    key,
-  };
-  span.innerText = `${key}`;
-  buttonExit.innerText = "❌";
-  buttonExit.id = "button__exit";
-  li.appendChild(span);
-  li.appendChild(buttonExit);
-  li.id = index;
-  toDoListComplete.appendChild(li);
-  toDosDone.push(newToDo);
-  // console.log(toDos);
-  saveToDoComplete();
-  buttonExit.addEventListener("click", removeToDoComplete);
+function saveToDo() {
+  localStorage.setItem(TODO_VALUE, JSON.stringify(toDos));
 }
 
 function paintToDoSaved(key, index) {
@@ -169,6 +128,38 @@ function paintToDoSaved(key, index) {
   buttonExit.innerText = "❌";
   buttonExit.id = "button__exit";
   li.appendChild(span);
+  li.appendChild(div);
+  div.appendChild(buttonComplete);
+  div.appendChild(buttonExit);
+  div.id = "buttonContainer";
+  li.id = index;
+  toDoListUncomplete.appendChild(li);
+  toDos.push(newToDo);
+  // console.log(toDos);
+  saveToDo();
+  buttonExit.addEventListener("click", removeToDo);
+  buttonComplete.addEventListener("click", completeToDo);
+  buttonComplete.addEventListener("click", removeToDo);
+}
+
+function paintToDoInitial(currentToDo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  const div = document.createElement("div");
+  const buttonComplete = document.createElement("button");
+  const buttonExit = document.createElement("button");
+  const index = Date.now();
+  newToDo = {
+    index: index,
+    key: currentToDo,
+  };
+  span.innerText = `${currentToDo}`;
+  buttonComplete.innerText = "✔️";
+  buttonComplete.id = "button__complete";
+  buttonExit.innerText = "❌";
+  buttonExit.id = "button__exit";
+  li.appendChild(span);
+  li.appendChild(buttonComplete);
   li.appendChild(div);
   div.appendChild(buttonComplete);
   div.appendChild(buttonExit);
@@ -207,6 +198,7 @@ function loadToDo() {
 
 function init() {
   loadToDo();
+
   toDoForm.addEventListener("submit", (event) => {
     event.preventDefault();
     const toDoValue = toDoInput.value;
